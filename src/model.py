e6857fdab55658c2bdeb52067cae92d43a259694
llevy1 <leahshohet@hotmail.co.uk>
2024-11-21 15:56:58 +0000

    reformatted file

--- src/model.py
+++ src/model.py
@@ -3,6 +3,7 @@ import pandas as pd
 # from sklearn.model_selection import train_test_split
 # from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import OrdinalEncoder
+
 # from sklearn.svm import LinearSVC
 
 
